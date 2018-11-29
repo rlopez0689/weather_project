e6258b01492740b83737855bf19c0f0a1e93601d
Rodrigo Lopez <rodrigo.lopez@digitalonus.com>
2018-11-28 20:52:11 -0600

    Add env variables for db

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -68,6 +68,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'weather_project.wsgi.application'
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('PORT')
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
